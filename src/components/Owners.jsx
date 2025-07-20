ed916f3cb383d71c44ccb166baf3b9f8398213af
Michael <mmehta1@wgu.edu>
2025-07-20 12:30:32 -0700

    Owners are now retrieved from the backend and displayed in a table

--- src/components/Owners.jsx
+++ src/components/Owners.jsx
@@ -1,6 +1,43 @@
+import { getOwners } from '../services/ownerService'
+import { useEffect, useState } from 'react'
+import {DataGrid} from '@mui/x-data-grid'
+
 const Owners = () => {
+
+    const [owners, setOwners] = useState([])
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const data = await getOwners()
+                setOwners(data)
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchData()
+
+
+    }, [])
+
+    const columns = [
+        {field:'id', headerName:'ID', width: 50},
+        {field:'name', headerName:'Name', width: 150},
+        {field:'phone', headerName:'Phone', width: 150}
+    ]
+
+    console.log(owners)
+
     return (
+        <>
         <h1>Owners</h1>
+        <DataGrid rows={owners} columns={columns}></DataGrid>
+        </>
+        
+        
     )
 }
 
---
+++ src/services/ownerService.jsx
@@ -0,0 +1,11 @@
+export const getOwners = async () => {
+    try {
+        const response = await fetch("http://localhost:8000/clinic/owners/")
+        const parsedResponse = await response.json()
+        return parsedResponse
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+
+}
