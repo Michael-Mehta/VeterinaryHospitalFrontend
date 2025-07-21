229bce21ef335bdc3019a62a396bfb5ae680911b
Michael <mmehta1@wgu.edu>
2025-07-20 19:52:49 -0700

    finished the REST functionality for the Owners. Now need to implement it for the other parts

--- src/components/OwnerModal.jsx
+++ src/components/OwnerModal.jsx
@@ -1,9 +1,82 @@
-import {Modal} from '@mui/material'
+import { Modal, Box, Typography, TextField, Button } from '@mui/material'
+import { createOwners, updateOwners } from '../services/ownerService'
+import { useState, useEffect } from 'react'
 
-const OwnerModal = ({modalOpen, setModalOpen, selectedOwner}) => {
-    return(
-        <Modal open = {modalOpen} onClose = {() => setModalOpen(false)}>
-            <p>Test Modal</p>
+const OwnerModal = ({ modalOpen, setModalOpen, selectedOwner }) => {
+
+    const isCreate = selectedOwner.id === undefined
+    const [formdata, setFormData] = useState({ name: "", phone: "" })
+
+
+    useEffect(() => {
+
+        setFormData({ name: selectedOwner.name || "", phone: selectedOwner.phone || "" })
+
+    }, [selectedOwner, setModalOpen])
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        bgcolor: "white",
+        width: 300,
+        height: 300,
+        transform: "translate(-50%, -50%)",
+        p: 3,
+        borderRadius: 1,
+        boxShadow: 24
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            if(isCreate)
+            {
+                await createOwners(formdata)
+
+            }else
+            {
+                await updateOwners(selectedOwner.id, formdata)
+
+            }
+            setModalOpen(false)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+            <Box sx={style}>
+                <Typography>{isCreate ? "Creating Owner" : "Updating Owner"}</Typography>
+                <form onSubmit={handleSubmit}>
+                    <TextField
+                        label="Name"
+                        value={formdata.name}
+                        onChange={(e) =>
+                            setFormData({ ...formdata, name: e.target.value })
+                        }
+                        fullWidth
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        label="Phone"
+                        value={formdata.phone}
+                        onChange={(e) =>
+                            setFormData({ ...formdata, phone: e.target.value })
+                        }
+                        fullWidth
+                        sx={{ mb: 2 }}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                    >Create</Button>
+                </form>
+            </Box>
+
         </Modal>
     )
 }
--- src/components/Owners.jsx
+++ src/components/Owners.jsx
@@ -3,12 +3,35 @@ import { useEffect, useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid'
 import OwnerModal from './OwnerModal'
 import { Button } from '@mui/material'
+import { deleteOwners } from '../services/ownerService'
 
 const Owners = () => {
 
     const [owners, setOwners] = useState([])
     const [modalOpen, setModalOpen] = useState(false)
-    const [selectedOwner, setSelectedOwner] = useState({ name: '', phone: '' })
+    const [selectedOwner, setSelectedOwner] = useState({})
+
+    const handleDelete = async (ownerId) => {
+
+        try{
+            const success = await deleteOwners(ownerId)
+
+            if(success)
+            {
+                setOwners(owners.filter(owner => owner.id !== ownerId))
+                console.log("Owner deleted successfully")
+
+            }else
+            {
+                console.error("Failed to delete owner")
+            }
+
+        }catch(error)
+        {
+            console.log(error)
+        }
+
+    }
 
     useEffect(() => {
 
@@ -30,13 +53,31 @@ const Owners = () => {
     const columns = [
         { field: 'id', headerName: 'ID', width: 50 },
         { field: 'name', headerName: 'Name', width: 150 },
-        { field: 'phone', headerName: 'Phone', width: 150 }
+        { field: 'phone', headerName: 'Phone', width: 150 },
+        { field: 'actions', headerName: 'Actions', width: 250, renderCell: (params) => (
+            <>
+            <Button
+             variant="outlined"
+             onClick = {() => {
+                setSelectedOwner(params.row)
+                setModalOpen(true)
+             }}
+             >Edit</Button>
+             <Button
+             variant="outlined"
+             color="error"
+             onClick = {() => {
+                handleDelete(params.row.id)
+             }}
+             >Delete</Button>
+            </>
+        ) }
     ]
 
 
 
     const createOwner = () => {
-        setSelectedOwner({ name: '', phone: '' })
+        setSelectedOwner({})
         setModalOpen(true)
     }
 
